68cef2f99b0d2f4565e931dc04893fb5003dc7a3
Kun Fang <mikael8fun@gmail.com>
2023-11-06 15:29:47 +0800

    Update tkinterui.py

--- src/tkinterui.py
+++ src/tkinterui.py
@@ -103,8 +103,8 @@ class Root(Tk):
         download_file('https://raw.githubusercontent.com/mikaelfun/Intune-IME-Project/main/src/logging%20keyword%20table.json', 'logging keyword table.json')
 
         download_file(
-        'https://raw.githubusercontent.com/mikaelfun/Intune-IME-Project/main/src/logging%20keyword%20table.json',
-        'logging keyword table.json')
+        'https://raw.githubusercontent.com/mikaelfun/Intune-IME-Project/main/src/imeinterpreter.cp311-win_amd64.pyd',
+        'imeinterpreter.cp311-win_amd64.pyd')
 
     def turn_on(self):
         self.enable_full_log.set(True)
@@ -146,8 +146,8 @@ class Root(Tk):
         self.label.configure(text=self.log_folder_name)
 
     def start_analyze(self, ime_log_folder_path):
-        # importlib.reload(imeinterpreter)
-        ime_interpreter_object = imeinterpreter .ImeInterpreter(ime_log_folder_path)
+        importlib.reload(imeinterpreter)
+        ime_interpreter_object = imeinterpreter.ImeInterpreter(ime_log_folder_path)
         processed_log = ime_interpreter_object.generate_ime_interpreter_log_output(self.on_off_button.var.get())
         self.text_output.delete("1.0", "end")
         self.text_output.insert(END, processed_log)
